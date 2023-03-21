d52a187cc4782738cdea2c9c3c3b5f9d3dcb05db
MyuTsu <gaetan.langlois31@gmail.com>
2023-03-21 10:45:36 +0100

    Page Index et Domain

---
+++ src/main/kotlin/edu/spring/btp/controllers/IndexController.kt
@@ -0,0 +1,43 @@
+package edu.spring.btp.controllers
+
+import edu.spring.btp.entities.Domain
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/")
+class IndexController {
+
+    @Autowired
+    lateinit var userRepository: edu.spring.btp.repositories.UserRepository
+
+    @Autowired
+    lateinit var domainRepository: edu.spring.btp.repositories.DomainRepository
+
+    @Autowired
+    lateinit var complaintRepository: edu.spring.btp.repositories.ComplaintRepository
+
+    @Autowired
+    lateinit var providerRepository: edu.spring.btp.repositories.ProviderRepository
+
+    @RequestMapping("")
+    fun showIndex(modelMap: ModelMap): String {
+        val domainName = "Root"
+        val rootDomain: Domain = domainRepository.findByName(domainName)
+        modelMap["domain"] = rootDomain
+        modelMap["name"] = domainName
+        return "index"
+    }
+
+    @RequestMapping("/domain/{name}")
+    fun showDomain(@PathVariable name: String, modelMap: ModelMap): String {
+        val domain: Domain = domainRepository.findByName(name)
+        modelMap["domain"] = domain
+        modelMap["name"] = name
+        return "domain"
+    }
+
+}
--- src/main/kotlin/edu/spring/btp/repositories/DomainRepository.kt
+++ src/main/kotlin/edu/spring/btp/repositories/DomainRepository.kt
@@ -3,7 +3,9 @@ package edu.spring.btp.repositories
 import edu.spring.btp.entities.Domain
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DomainRepository:JpaRepository<Domain, Int> {
     @Query(nativeQuery = true,value="SELECT * FROM \"domain\" ORDER BY rand() LIMIT 1")
     fun getRandomDomain(): Domain
@@ -11,6 +13,18 @@ interface DomainRepository:JpaRepository<Domain, Int> {
     @Query(nativeQuery = true,value="SELECT * FROM \"domain\" INNER JOIN \"domain_providers\" ON \"providers_id\"=:providerId ORDER BY rand() LIMIT 1")
     fun getRandomDomainFromProvider(providerId:Int): Domain
 
+    @Query(nativeQuery = true, value = "SELECT * FROM \"domain\" WHERE \"name\" = :name")
+    fun findByName(name: String):Domain
+
+    @Query(nativeQuery = true, value = "SELECT * FROM \"domain\" WHERE \"parent_id\" IN (SELECT \"id\" FROM \"domain\" where \"name\" = :name)")
     fun findByParentName(name:String):List<Domain>
+
+    @Query(nativeQuery = true, value = "SELECT * FROM \"domain\" WHERE \"parent\" IS NULL")
+    fun findWithoutParent(name: String):List<Domain>
+
+    @Query(nativeQuery = true, value = "SELECT * FROM \"domain\" WHERE \"parent_id\" IN (SELECT \"id\" FROM \"domain\")")
+    fun findByParentId(id: Int):List<Domain>
+//
+//    fun findRandomByParentId(id: Int): Domain
 
 }
